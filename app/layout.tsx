7edde6a6c7cfffbd1170cc74f5fbc094d339b99b
Francis Tran <xst-tran6832@stthomas.edu>
2026-06-08 19:07:41 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,34 +14,34 @@ export const metadata: Metadata = {
   },
   title: "Francis Tran",
   description:
-    "Francis Tran is a Full-Stack Engineer with 4+ years of experience.",
+    "Francis Tran is a Full-Stack Developer with 4+ years of experience.",
   keywords:
-    "Software Engineer, Web Development, Game Development, Machine Learning, AI",
+    "Software Engineer, Full-Stack Developer, Web Development, Machine Learning, AI",
   openGraph: {
     locale: "en_US",
     siteName: "Francis Tran",
     type: "website",
     title: "Francis Tran",
     description:
-      "Francis Tran is a Full-Stack Engineer with 4+ years of experience.",
+      "Francis Tran is a Full-Stack Developer with 4+ years of experience.",
     url: "https://francistr.github.io",
     images: [
       {
         url: "/OGavatar.png",
         width: 1200,
         height: 630,
-        alt: "Francis Tran - Full-Stack Engineer",
+        alt: "Francis Tran - Full-Stack Developer",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Francis Tran",
-    description: "Francis Tran is a Full-Stack Engineer with 4+ years of professional experience.",
+    description: "Francis Tran is a Full-Stack Developer with 4+ years of overall professional experience.",
     images: ["/OGavatar.png"],
   },
   other: {
-    "theme-color": "#000000",
+    "theme-color": "#005da8",
   },
 };
 
